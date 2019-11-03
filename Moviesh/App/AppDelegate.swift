bc853f9b0db31da3de958e3db5584a3033e17236
Filip Miladinovic <f.miladinovic@netconomy.net>
2019-11-03 12:42:10 +0100

    initial commit

---
+++ Moviesh/App/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Moviesh
+//
+//  Created by Filip Miladinovic on 5/11/19.
+//  Copyright © 2019 Filip Miladinovic. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+
+class AppDelegate: UIResponder {
+
+    var window: UIWindow?
+}
+
+
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Moviesh/RootVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Moviesh
+//
+//  Created by Filip Miladinovic on 5/11/19.
+//  Copyright © 2019 Filip Miladinovic. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchMovies()
+    }
+
+
+    // Fetch resources - Movies
+
+    func fetchMovies() {
+        
+    }
+    
+}
+
